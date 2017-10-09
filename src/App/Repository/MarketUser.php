27ca08f84428edf3bab778ebabc1b7421ae0df06
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2017-10-09 16:48:28 -0400

    Refactored sql-statement to prepared statements

--- src/App/Repository/MarketUser.php
+++ src/App/Repository/MarketUser.php
@@ -16,8 +16,10 @@ class MarketUser extends EntityRepository
     function fetchUserMarketDealIds($userId)
     {
         $dql = 'SELECT u FROM \App\Entity\MarketUser u INNER JOIN deal_market_user WHERE deal_market_user.market_user_id = ?1';
-        $sql = "SELECT deal_id FROM deal_market_user WHERE  market_user_id = $userId";
+        $sql = "SELECT deal_id FROM deal_market_user WHERE  market_user_id = ?";
         $stmt= $this->getEntityManager()->getConnection()->prepare($sql);
+        $stmt->bindValue(1, $userId);
+        $stmt->execute();
         $results = $this->getEntityManager()->getConnection()->exec($sql);
         $query = $this->getEntityManager()->createQuery($dql);
         $query->setParameter(1, $userId);
