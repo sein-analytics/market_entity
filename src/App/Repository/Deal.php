f05e44ca680b957aeb7af93a9bbde503bdac8582
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2017-10-11 16:47:46 -0400

    Made JOIN more verbose

--- src/App/Repository/Deal.php
+++ src/App/Repository/Deal.php
@@ -17,7 +17,9 @@ class Deal extends EntityRepository
 
     public function fetchUserDealsFromIds(array $ids)
     {
-        $sql = 'SELECT *, MarketUser.first_name, MarketUser.last_name FROM Deal INNER JOIN MarketUser ON Deal.user_id = MarketUser.id ' .
+        $sql = 'SELECT Deal.id, Deal.issuer_id, Deal.auction_type.id, Deal.asset_type_id, Deal.bid_type_id, Deal.issue, Deal.cut_off_date, Deal.closing_date,' .
+                'Deal.current_balance, Deal.views, ' .
+                'MarketUser.first_name, MarketUser.last_name FROM Deal INNER JOIN MarketUser ON Deal.user_id = MarketUser.id ' .
                 'WHERE Deal.status_id = 1 AND Deal.id IN (?) ORDER BY Deal.id ASC';
         $results = $this->fetchByIntArray($this->getEntityManager(), $ids, $sql);
         return $results;
