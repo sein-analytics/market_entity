176f3b314a51a9c532c1116e848dae03c8d91679
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2017-11-30 16:51:12 -0500

    Refactored getEntityManager() to $this->em

--- src/App/Repository/Deal.php
+++ src/App/Repository/Deal.php
@@ -28,7 +28,7 @@ class Deal extends EntityRepository
                 'Deal.current_balance, Deal.views, ' .
                 'MarketUser.first_name, MarketUser.last_name FROM Deal INNER JOIN MarketUser ON Deal.user_id = MarketUser.id ' .
                 'WHERE Deal.status_id = 1 AND Deal.id IN (?) ORDER BY Deal.id ASC';
-        $results = $this->fetchByIntArray($this->getEntityManager(), $ids, $sql);
+        $results = $this->fetchByIntArray($this->em, $ids, $sql);
         return $results;
     }
 
