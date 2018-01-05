b032396b88fcf55005793ceab0baff6b9bee0402
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-01-05 11:58:24 -0500

    Refactored fetching deals to get closed deals also when in activity module

--- src/App/Repository/Deal.php
+++ src/App/Repository/Deal.php
@@ -43,14 +43,16 @@ class Deal extends EntityRepository implements SqlManagerTraitInterface
 
     /**
      * @param array $ids
+     * @param bool $isMarket
      * @return array|bool
      */
-    public function fetchUserMarketDealsFromIds(array $ids)
+    public function fetchUserMarketDealsFromIds(array $ids, $isMarket=true)
     {
         $sql = 'SELECT Deal.id, Deal.issuer_id, Deal.auction_type_id, Deal.asset_type_id, Deal.bid_type_id, Deal.issue, Deal.cut_off_date, Deal.closing_date, ' .
-                'Deal.current_balance, Deal.views, ' .
-                'MarketUser.first_name, MarketUser.last_name FROM Deal INNER JOIN MarketUser ON Deal.user_id = MarketUser.id ' .
-                'WHERE Deal.status_id = 1 AND Deal.id IN (?) ORDER BY Deal.id ASC';
+                'Deal.current_balance, Deal.views, Deal.status_id ' .
+                'MarketUser.first_name, MarketUser.last_name FROM Deal INNER JOIN MarketUser ON Deal.user_id = MarketUser.id ';
+        if ($isMarket){ $sql .= 'WHERE Deal.status_id = 1 AND Deal.id IN (?) ORDER BY Deal.id ASC'; }
+        else { $sql .= 'WHERE Deal.status_id = 1 OR Deal.status_id = 2 AND Deal.id IN (?) ORDER BY Deal.id ASC'; }
         $results = $this->fetchByIntArray($this->em, $ids, $sql);
         return $results;
     }
