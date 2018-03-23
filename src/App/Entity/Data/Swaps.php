965a347405a2f305541ef6f85094dd13d47ad439
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-03-23 14:48:31 -0400

    Refactored annotation to reflect Swaps as repository.

--- src/App/Entity/Data/Swaps.php
+++ src/App/Entity/Data/Swaps.php
@@ -11,7 +11,7 @@ namespace App\Entity\Data;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="\App\Repository\Data\Swap")
+ * @ORM\Entity(repositoryClass="\App\Repository\Data\Swaps")
  * @ORM\Table(name="Swaps")
  */
 class Swaps
