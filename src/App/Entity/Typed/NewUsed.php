0603a87ea4c2e9190b6be4e6c7cb6a23aeba8515
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-04-15 15:24:00 -0400

    Added NewUsed and YearBuilt entities to Typed.

---
+++ src/App/Entity/Typed/NewUsed.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ac1189
+ * Date: 4/15/18
+ * Time: 3:20 PM
+ */
+
+namespace App\Entity\Typed;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="NewUsed")
+ */
+class NewUsed extends MappedTypeAbstract
+{
+
+}
