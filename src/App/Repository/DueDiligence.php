0be28faf0a73d214ecdf35bab9c70f54afdae972
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-07-11 16:21:12 -0400

    Added additional methods for DD data retrievals.

--- src/App/Repository/DueDiligence.php
+++ src/App/Repository/DueDiligence.php
@@ -38,4 +38,49 @@ class DueDiligence extends EntityRepository
         }
         return $results;
     }
+
+    /**
+     * @param array $ddIds
+     * @return array
+     */
+    public function fetchDealFileDataByDdIds(array $ddIds)
+    {
+        $sql = 'SELECT loan_id, deal_file_id, doc_type_id, file_name FROM deal_file_due_diligence ' .
+            'LEFT JOIN `DealFile` file on file.id = deal_file_due_diligence.deal_file_id' .
+            'WHERE due_diligence_id IN (?) ORDER BY loan_id, doc_type_id ASC';
+        $stmt = $this->getEntityManager()->getConnection()->executeQuery($sql,
+            array($ddIds),
+            array(Connection::PARAM_INT_ARRAY)
+        );
+        $results = $stmt->fetchAll(Query::HYDRATE_ARRAY);
+        return $results;
+    }
+
+    /**
+     * @param array $ddIds
+     * @return array
+     */
+    public function fetchDdIssuesDataByDdIds(array  $ddIds)
+    {
+        $sql = 'SELECT id, due_diligence_id, status_id, file_id, open_date, closed_date FROM DueDiligenceIssue ' .
+            'WHERE due_diligence_id IN (?) ORDER BY id, due_diligence_id ASC';
+        $stmt = $this->getEntityManager()->getConnection()->executeQuery($sql,
+            array($ddIds),
+            array(Connection::PARAM_INT_ARRAY)
+        );
+        $results = $stmt->fetchAll(Query::HYDRATE_ARRAY);
+        return $results;
+    }
+
+    public function fetchMsgIssuesDataByIssueIdsLoanIds(array $issueIds, array $loanIds)
+    {
+        $sql = 'SELECT id as msg_id, loan_id, issue_id, type_id, status_id, priority_id, date, subject, message FROM Message ' .
+            'WHERE issue_id IN (?) AND loan_id IN (?)';
+        $stmt = $this->getEntityManager()->getConnection()->executeQuery($sql,
+            array($issueIds, $loanIds),
+            array(Connection::PARAM_INT_ARRAY, Connection::PARAM_INT_ARRAY)
+        );
+        $results = $stmt->fetchAll(Query::HYDRATE_ARRAY);
+        return $results;
+    }
 }
