7988cce5deaac1a51f59a233dae64d83f5b0e732
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-07-25 17:38:50 -0400

    Added DueDilIssueStatus repo.

--- src/App/Entity/DueDilIssueStatus.php
+++ src/App/Entity/DueDilIssueStatus.php
@@ -11,7 +11,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="\App\Repository\DueDilIssueStatus")
  * @ORM\Table(name="DueDilIssueStatus")
  */
 class DueDilIssueStatus
---
+++ src/App/Repository/DueDilIssueStatus.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ac1189
+ * Date: 7/25/18
+ * Time: 5:36 PM
+ */
+
+namespace App\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class DueDilIssueStatus extends EntityRepository
+{
+}
