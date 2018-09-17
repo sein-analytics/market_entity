e13a212694b9696aef2aa7d3b9a454c6c47e12d8
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-09-17 17:52:17 -0400

    Added method fetchAllIssueIds.

--- src/App/Repository/DueDiligenceIssue.php
+++ src/App/Repository/DueDiligenceIssue.php
@@ -10,17 +10,36 @@ namespace App\Repository;
 
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Query;
+use Illuminate\Support\Facades\Log;
 
 class DueDiligenceIssue extends EntityRepository
 {
     public function updateIssueStatus(int $status, int  $id)
     {
         $sql = "UPDATE DueDiligenceIssue SET status_id = $status WHERE id = $id";
-        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        try {
+            $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        } catch (\Exception $e){
+            return false;
+        }
         $result = $stmt->execute();
         if ($result)
             return true;
         else
             return false;
+    }
+
+    public function fetchAllIssueIds()
+    {
+        $sql = "SELECT id FROM DueDiligenceIssue";
+        try{
+            $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        } catch (\Exception $exception){
+            return false;
+        }
+        $result = $stmt->fetchAll(Query::HYDRATE_ARRAY);
+        return $result;
     }
 }
