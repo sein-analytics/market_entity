452b6818c1dc5d3e0e72c8280701682b0471430c
tmaior <tmaior@ezops.com.br>
2024-02-13 13:14:17 -0300

    feat: created ContractStatus entity and his properties get and setters (ContracStatus.php entity)

---
+++ src/App/Entity/ContractStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * @ORM\Table(name="ContractStatus")
+ */
+class ContractStatus
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     **/
+    protected int $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     * @var string
+     */
+    protected string $status;
+
+    /**
+     * @return int
+     */
+    public function getId(): int { return $this->id; }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string { return $this->status; }
+
+    /**
+     * @param string
+     */
+    public function setStatus(string $status):void { $this->status = $status; }
+    
+}
