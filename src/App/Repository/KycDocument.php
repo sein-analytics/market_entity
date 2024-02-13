606c8832a7907fdf5eeda0275450fa017bec8ee9
tmaior <tmaior@ezops.com.br>
2024-02-13 18:05:31 -0300

    feat: created repository with methods to create and update document status (KycDocument.php repository)

---
+++ src/App/Repository/KycDocument.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Repository\KycDocument\KycDocumentAbstract;
+use App\Service\FetchingTrait;
+use App\Service\FetchMapperTrait;
+
+class KycDocument extends KycDocumentAbstract
+{
+
+    use FetchMapperTrait, FetchingTrait;
+
+    private string $insertKycDocumentSql = "INSERT INTO KycDocument VALUE (null, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    private string $updateKycDocumentStatusSql = "UPDATE KycDocument SET contract_status_id=? WHERE id=?";
+
+    public function insertNewKycDocument(array $params):mixed
+    {
+        if (array_key_exists(self::DC_QRY_ID_KEY, $params))
+        unset($params[self::DC_QRY_ID_KEY]);
+
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $this->insertKycDocumentSql,
+            self::EXECUTE_MTHD,
+            array_values($params)
+        );
+    }
+
+    public function updateKycDocumentStatus(int $statusId, int $documentId):mixed
+    {
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $this->updateKycDocumentStatusSql,
+            self::EXECUTE_MTHD,
+            [$statusId, $documentId]
+        ); 
+    }
+
+}
