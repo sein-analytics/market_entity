e2f0a377d74fb2237420bc964980965699591b2c
tmaior <tmaior@ezops.com.br>
2024-05-21 17:16:25 -0300

    feat: created repository and added service imports (ContractSignature.php repository)

---
+++ src/App/Repository/ContractSignature.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repository;
+
+use App\Service\FetchingTrait;
+use App\Service\FetchMapperTrait;
+
+class ContractSignature
+{
+
+    use FetchMapperTrait, FetchingTrait;
+
+}
