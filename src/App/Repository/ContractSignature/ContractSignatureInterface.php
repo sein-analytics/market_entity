059e818d86f24ebe67ce6641aeadb4ac74463983
tmaior <tmaior@ezops.com.br>
2024-05-21 17:31:59 -0300

    feat: created interface and defined consts to db columns and response keys (ContractSignatureInterface.php interface)

---
+++ src/App/Repository/ContractSignature/ContractSignatureInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository\ContractSignature;
+
+interface ContractSignatureInterface
+{
+    const CS_QRY_SENDER_ID_KEY = 'sender_id';
+
+    const CS_QRY_RECEIVER_ID_KEY = 'receiver_id';
+
+    const CS_QRY_CONTRACT_STATUS_ID_KEY = 'contract_status_id';
+
+    const CS_QRY_SENDER_SIGNATURE_KEY = 'sender_signature';
+
+    const CS_QRY_RECEIVER_SIGNATURE_KEY = 'receiver_signature';
+
+    const CS_QRY_SIGNATURE_ID_KEY = 'signature_id';
+
+    const CS_QRY_PUBLIC_ID_KEY = 'public_id';
+
+    const CS_SENDER_ID_KEY = 'senderId';
+
+    const CS_RECEIVER_ID_KEY = 'receiverId';
+
+    const CS_CONTRACT_STATUS_ID_KEY = 'contractStatusId';
+
+    const CS_SENDER_SIGNATURE_KEY = 'senderSignature';
+
+    const CS_RECEIVER_SIGNATURE_KEY = 'receiverSignature';
+
+    const CS_SIGNATURE_ID_KEY = 'signatureId';
+
+    const CS_PUBLIC_ID_KEY = 'publicId';
+
+}
