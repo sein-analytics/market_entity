401b6b931fba94936a41a92de2c14604bc9e7743
tmaior <tmaior@ezops.com.br>
2024-05-21 17:40:26 -0300

    feat: created function to insert new entry in ContractSignature table (ContractSignature.php repository)

--- src/App/Repository/ContractSignature.php
+++ src/App/Repository/ContractSignature.php
@@ -4,10 +4,27 @@ namespace App\Repository;
 
 use App\Service\FetchingTrait;
 use App\Service\FetchMapperTrait;
+use App\Repository\DbalStatementInterface;
+use Doctrine\ORM\EntityRepository;
 
-class ContractSignature
+class ContractSignature extends EntityRepository implements DbalStatementInterface
 {
 
     use FetchMapperTrait, FetchingTrait;
+
+    private string $insertContractSignatureSql = "INSERT INTO ContractSignature VALUE (null, ?, ?, ?, ?, ?, ?, ?)";
+
+    public function insertNewContractSignature(array $params):mixed
+    {
+        if (array_key_exists(self::QUERY_JUST_ID, $params))
+        unset($params[self::QUERY_JUST_ID]);
+
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $this->insertContractSignatureSql,
+            self::EXECUTE_MTHD,
+            array_values($params)
+        );
+    }
 
 }
