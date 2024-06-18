0936549a5133023c00d05fcebbf724be45f4113b
Guilherme Anjos <guilherme.anjos@ezops.cloud>
2024-06-18 10:55:58 -0300

    feat: created repository, created function to insert multiple kyc document requests by provided community user id, issuer id, asset type, description, date and bids list (KycDocRequest.php repository insertMultiKycDocRequests function)

---
+++ src/App/Repository/KycDocRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repository;
+
+use App\Repository\KycDocument\KycDocumentAbstract;
+use App\Service\FetchingTrait;
+use App\Service\FetchMapperTrait;
+
+class KycDocRequest extends KycDocumentAbstract
+{
+
+    use FetchMapperTrait, FetchingTrait;
+
+    private string $insertMultiKycDocRequestsSql = "INSERT INTO KycDocRequest " .
+        "(`community_user_id`, `community_issuer_id`, `user_id`, `issuer_id`, `kyc_type_id`, `kyc_asset_type_id`, `description`, `date`, `bid_id`)" .
+        " VALUES";
+
+
+    public function insertMultiKycDocRequests(int $communityIssuerId, int $communityUserId, int $assetTypeId, int $kycTypeId, string $description, \DateTime $date, array $bids)
+    {
+        $base = $this->insertMultiKycDocRequestsSql;
+        $insertCount = 0;
+        foreach ($bids as $bid) {
+            $insertCount++;
+            $base = $base . PHP_EOL .
+                '(' .
+                $communityUserId . ',' . $communityIssuerId . ',' .
+                $bid['userId'] . ',' . $bid['issuerId'] . ',' . $kycTypeId . ',' .
+                $assetTypeId . ',' . $description . ',' . $date . ',' . $bid['bidId'] .
+                ')' . ($insertCount == count($bids) ? ';' : ',');
+        }
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $base,
+            self::EXECUTE_MTHD,
+            []
+        );
+    }
+}
