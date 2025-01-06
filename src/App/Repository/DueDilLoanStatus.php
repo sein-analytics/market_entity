4d5fc27edfdda261b51ef3cbcf944437c6cf6372
Guilherme Anjos <guilherme.anjos@ezops.cloud>
2025-01-06 14:05:00 -0300

    feat: created function to fetch a due dil loan status row and some data from tables who made a relationship with DueDilLoanStatus table (user data, deal data, due diligence parent datas) using provided due diligence id and loan id to the query (DueDilLoanStatus.php repository fetchDueDilLoanStatusByDdAndLn function)

--- src/App/Repository/DueDilLoanStatus.php
+++ src/App/Repository/DueDilLoanStatus.php
@@ -237,4 +237,23 @@ class DueDilLoanStatus extends DueDiligenceAbstract
         $baseLogger = self::BASE_LOGGER_ARRAY;
         return addslashes(json_encode([$baseLogger]));
     }
+
+    public function fetchDueDilLoanStatusByDdAndLn(int $ddId, int $lnId)
+    {
+        $baseQry = "SELECT ddLnStatus.dd_id AS ddId, ddLnStatus.ln_id AS loanId, ddLnStatus.status_id AS statusId, " . 
+            "ddLnStatus.issues_count AS issuesCount, ddLnStatus.last_modified AS lastModified, ddLnStatus.logger, " .
+            "dds.parent_id AS ddParentId, dds.deal_id AS dealId, dds.user_id AS userId, user.issuer_id AS issuerId " .
+            "FROM DueDilLoanStatus AS ddLnStatus " .
+            "LEFT JOIN DueDiligence AS dds ON dds.id = ddLnStatus.dd_id " .
+            "LEFT JOIN MarketUser AS user ON user.id = dds.user_id " .
+            "WHERE ddLnStatus.dd_id=? AND ddLnStatus.ln_id=?";
+
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $baseQry,
+            self::FETCH_ASSO_MTHD,
+            [$ddId, $lnId]
+        );
+    }
+
 }
