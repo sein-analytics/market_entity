baa1355f9b2c957244be1b31247eccdcb4189e7b
tmaior <tlmaior@gmail.com>
2025-06-03 15:05:51 -0300

    feat: added more params to add in insert deal contract sql string, added functions to fetch by id, fetch by props, fetch by asset, update and delete a DealContract (DealContract.php repository)

--- src/App/Repository/DealContract.php
+++ src/App/Repository/DealContract.php
@@ -11,9 +11,15 @@ class DealContract extends DealContractAbstract
 
     use FetchMapperTrait, FetchingTrait;
 
-    private string $insertDealContractSql = "INSERT INTO DealContract VALUE (null, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private string $insertDealContractSql = "INSERT INTO DealContract VALUE (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     private string $updateDealContractStatusSql = "UPDATE DealContract SET contract_status_id=? WHERE id=?";
+
+    private string $fetchDealContractByIdSql = "SELECT * FROM DealContract WHERE id=?";
+
+    private string $fetchDocumentByAssetIdSql = "SELECT * FROM DealContract WHERE asset_id=?";
+
+    private string $deleteFileByIdSql = "DELETE FROM DealContract WHERE id=?";
 
     public function insertNewDealContract(array $params):mixed
     {
@@ -28,6 +34,31 @@ class DealContract extends DealContractAbstract
         );
     }
 
+    public function updateDealContractById(int $dealContractId, array $columnsValues)
+    {
+        $query = "UPDATE DealContract SET ";
+        $values = [];
+        $count = 0;
+
+        foreach($columnsValues as $key => $value) {
+            $count++;
+            $columnToSet = "$key=?" . ($count == count($columnsValues)
+                ? " " : ", ");
+            $query = $query . $columnToSet;
+            $values[] = $value;
+        }
+
+        $query = $query . "WHERE id=?";
+        $values[] = $dealContractId;
+
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $query,
+            self::EXECUTE_MTHD,
+            $values
+        );
+    }
+
     public function updateContractStatus(int $statusId, int $contractId):mixed
     {
         return $this->buildAndExecuteFromSql(
@@ -36,6 +67,63 @@ class DealContract extends DealContractAbstract
             self::EXECUTE_MTHD,
             [$statusId, $contractId]
         ); 
+    }
+
+    public function fetchDealContractById(int $dealContractId)
+    {
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $this->fetchDealContractByIdSql,
+            self::FETCH_ASSO_MTHD,
+            [$dealContractId]
+        );
+    }
+
+    public function fetchDealContractByProps(
+        int $dealId, 
+        int $docTypeId, 
+        int $userId, 
+        int $communityUserId,
+        ?int $bidId
+    ) {
+        $query = 
+            "SELECT * FROM DealContract WHERE deal_id=? AND doc_type_id=? " .
+                "AND user_id=? AND community_user_id=?";
+        $params = [$dealId, $docTypeId, $userId, $communityUserId];
+
+        if (!is_null($bidId)) {
+            $query = $query . " AND bid_id=?";
+            $params[] = $bidId;
+        } else {
+            $query = $query . " AND bid_id IS NULL";
+        }
+
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $query,
+            self::FETCH_ASSO_MTHD,
+            $params
+        );
+    }
+
+    public function fetchDocumentByAssetId(string $assetId):mixed
+    {
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $this->fetchDocumentByAssetIdSql,
+            self::FETCH_ASSO_MTHD,
+            [$assetId]
+        );
+    }
+ 
+    public function deleteFileById(int $dealContractId)
+    {
+        return $this->buildAndExecuteFromSql(
+            $this->getEntityManager(),
+            $this->deleteFileByIdSql,
+            self::EXECUTE_MTHD,
+            [$dealContractId]
+        );
     }
 
 }
