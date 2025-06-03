1648d4437e9fb522a3f2d47ebb687d4fe1d5d358
tmaior <tlmaior@gmail.com>
2025-06-03 15:04:14 -0300

    feat: created consts to user, community user, doc type, contract signature ids and date database columns and api keys names (DealContractInterface.php)

--- src/App/Repository/DealContract/DealContractInterface.php
+++ src/App/Repository/DealContract/DealContractInterface.php
@@ -23,6 +23,18 @@ interface DealContractInterface
     
     const DC_QRY_SECURE_URL_KEY = 'secure_url';
 
+    const DC_QRY_USER_ID_KEY = 'user_id';
+
+    const DC_QRY_COMMUNITY_USER_ID_KEY = 'community_user_id';
+
+    const DC_QRY_BID_ID_KEY = 'bid_id';
+
+    const DC_QRY_DOC_TYPE_ID_KEY = 'doc_type_id';
+
+    const DC_QRY_CONTRACT_SIGNATURE_ID = 'contract_signature_id';
+
+    const DC_QRY_DATE_KEY = 'date';
+
     const DC_CONTRACT_NAME_KEY = 'contractName';
 
     const DC_STATUS_KEY = 'status';
@@ -39,4 +51,15 @@ interface DealContractInterface
 
     const DC_FILE_NAME_KEY = 'fileName';
 
+    const DC_USER_ID_KEY = 'userId';
+
+    const DC_COMMUNITY_USER_KEY = 'communityUserId';
+
+    const DC_BID_ID_KEY = 'bidId';
+
+    const DC_DOC_TYPE_ID_KEY = 'docTypeId';
+
+    const DC_CONTRACT_SIGNATURE_ID_KEY = 'contractSignatureId';
+
+    const DC_DATE_KEY = 'date';
 }
