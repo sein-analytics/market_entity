1d654204c77101e84a0f6ecd3a42414fa2d3cba8
Your Name <you@example.com>
2025-07-23 19:01:55 +0000

    Refactoring fetchAllMappedTypeData, which is currently returning an empty array.'

---
+++ src/App/Repository/Typed/MappedTypeInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repository\Typed;
+
+interface MappedTypeInterface
+{
+    const ARM_INDEX_TYPE_DB = 'ArmIndexType';
+
+    const PURPOSE_TYPE_DB = 'PurposeType';
+
+    const OCCUPANCY_TYPE_DB = 'OccupancyType';
+
+    const STATUS_TYPE_DB = 'StatusType';
+
+    const LOAN_TYPE_DB = 'LoanType';
+
+    const ARM_INDEX_TYPE_PROP = 'armIndexType';
+
+    const PURPOSE_TYPE_PROP = 'purposeType';
+
+    const OCCUPANCY_TYPE_PROP = 'occupancyType';
+
+    const STATUS_TYPE_PROP = 'statusType';
+
+
+
+
+}
